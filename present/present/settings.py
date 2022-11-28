1755dc0ee9de390213cda4638fa6a8882e9518e6
i-sergh <seripp28@gmail.com>
2022-11-29 01:05:24 +0300

    some cleaning

--- present/present/settings.py
+++ present/present/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qy!6e=kb2nlt=o+4+5qva%r0_3^50xjxtm0n06&4jyq!r+m-mn'
+with open('secretkey.txt') as f:
+    key = f.read()
+print(key)    
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
