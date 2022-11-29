0abba5798d33336fd1922ff46ede95b378b71be3
i-sergh <seripp28@gmail.com>
2022-11-30 00:10:21 +0300

    come diffs in functional tests

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -12,12 +12,18 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_user_open_start_window(self):
+        # оочарованый произведегиями пользователь заходит на сайт 
         self.browser.get('http://localhost:8000')
-
+        
+        # Пользователь сразу подмечает заголовок страницы
         self.assertIn('Polly', self.browser.title)
+        
+        # Сразу же ему предлагается меню из возможных страниц к посещению
+        # ['Art galery', 'Posts', 'Contact', 'About']
+        #menu = self.browser.find_element()
+
         self.fail('End Test!')
 
-
+    
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-    
